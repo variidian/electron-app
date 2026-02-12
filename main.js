c9dfd879e530f0004260117e74881c373607136d
turdle <shuruiguo1@gmail.com>
2026-02-12 20:11:36 +1100

    stuff (does not work)

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   })
 
   win.loadFile('index.html')
